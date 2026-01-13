c5c089f320823686d41746d8325b716378e57a48
SraChumi <npramirezmora@gmail.com>
2026-01-13 17:45:11 +0100

    Añadida validación de errores en Datos_Simples_6

--- 01_Datos simples/Datos_Simples_6.py
+++ 01_Datos simples/Datos_Simples_6.py
@@ -1,10 +1,24 @@
-# 1. Entrada de datos
-# Usamos int() porque el enunciado pide un número entero
-n = int(input("Introduce un número entero positivo: "))
+def calcular_suma_gauss():
+    n = 0
 
-# 2. Cálculo usando la fórmula de Gauss
-# Usamos // para que el resultado sea un entero (división entera)
-suma = (n * (n + 1)) // 2
+    while True:
+        entrada = input("Introduce un número entero positivo: ")
+        try:
+            n = int(entrada)
+            if n > 0:
+                break
+            else:
+                print("Error: El número debe ser mayor que 0. Inténtalo de nuevo.")
+        except ValueError:
+            print(f"Error: '{entrada}' no es un número entero válido")
 
-# 3. Salida por pantalla
-print(f"La suma de los enteros desde 1 hasta {n} es: {suma}")
+    # Uso la fórmula de gauss y // para que el resultado sea un entero (división entera)
+    suma = (n * (n + 1)) // 2
+
+    # Muestro por pantalla
+    print(f"La suma de los enteros desde 1 hasta {n} es: {suma}")
+
+if __name__ == "__main__":
+    calcular_suma_gauss()
+
+
